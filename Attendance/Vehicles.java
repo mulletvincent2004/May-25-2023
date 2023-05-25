625cba4dc13221781f8909c7735196baa9f1cfb3
Mullet, Vincent Jay R <111546395+mulletvincent2004@users.noreply.github.com>
2023-05-26 00:32:04 +0800

    Create Vehicles.java

---
+++ Attendance/Vehicles.java
@@ -0,0 +1,23 @@
+package May252023;
+
+class Vehicles  {
+   
+    String Speed = "169 km/h";
+    String Color = "Black";
+    String Price  = "2,500,000 PHP";
+    String Brand = "Honda Civic";
+
+    String planeSpeed = "600 mph";
+    String planeColor = "Royal Blue and White";
+    String planePrice  = "25 million";
+    String planeBrand = " Airbus ";
+    
+    String yachtSpeed = "35 knots";
+    String yachtColor = "Pearl White";
+    String yachtPrice = "3.5 Million";
+    String yachtBrand = "Azimut Yacht";
+
+    void Stop () {
+     System.out.println("The Vehicles is stopping");	
+    }
+}
