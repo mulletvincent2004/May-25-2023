71f105e64534c6a9bb5ada9f70814143589ddce4
Mullet, Vincent Jay R <111546395+mulletvincent2004@users.noreply.github.com>
2023-05-26 00:33:18 +0800

    Create Fandango_Yacht.java

---
+++ Attendance/Fandango_Yacht.java
@@ -0,0 +1,11 @@
+package May252023;
+
+public class Fandango_Yacht extends Vehicles{
+    String MainSailColor = "Red";
+        
+    void Float() {
+    System.out.println("Main Sail Color: RED" );
+    }
+    
+ }
+    
